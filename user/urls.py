2b53aaeb43681e103980d8496100224e879ec7bf
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-22 18:00:13 +0000

    ADD: urls for user and token

---
+++ user/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from user import views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("create/", views.CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
