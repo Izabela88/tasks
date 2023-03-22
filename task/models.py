1bfd007e7bd5deadd9aaeb658e4efe186e74fc43
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-22 16:51:47 +0000

    UPDATE: models

--- task/models.py
+++ task/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from task.value_objects import TileStatus
+import uuid
 
 
 class BaseModel(models.Model):
     class Meta:
         abstract = True
 
-    id = models.UUIDField(primary_key=True, unique=True)
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     modified_at = models.DateTimeField(auto_now_add=True, null=False)
 
@@ -16,8 +17,8 @@ class Tile(BaseModel):
     status = models.CharField(max_length=50, choices=TileStatus.choices())
     launch_date = models.DateTimeField(auto_now_add=True, null=False)
 
-class TaskType(BaseModel):
-    name = models.CharField(max_length=255)
+class TaskType(models.Model):
+    name = models.CharField(primary_key=True, unique=True, max_length=255)
     
     def __str__(self):
         return self.name  
@@ -28,7 +29,7 @@ class Task(BaseModel):
     title = models.CharField(max_length=255)
     order = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    tile = models.ForeignKey(Tile, on_delete=models.CASCADE, null=False)
+    tile = models.ForeignKey(Tile, on_delete=models.CASCADE, null=False, related_name="tasks")
     task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, null=False, related_name="tasks")
 
 
