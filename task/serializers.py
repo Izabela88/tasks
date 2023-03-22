85911a9ffb66899490f394a9b4e0cdc9331aae7b
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-22 16:52:50 +0000

    ADD: serializers for Task and Tile models

---
+++ task/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from task.models import Task, Tile
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = ('__all__')
+        read_only_fields = ('id',)
+
+
+class TileSerializer(serializers.ModelSerializer):
+
+    tasks = TaskSerializer(read_only=True, many=True)
+    
+    class Meta:
+        model = Tile
+        fields = ('id', 'status', 'launch_date', 'created_at', 'modified_at', 'tasks')
+        read_only_fields = ('id', 'tasks')
