fc74331bbf6dcf5735b3a91092e54b0e2fe1790d
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-22 16:54:58 +0000

    ADD: urls

---
+++ task/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from task.views import TaskDetail, TaskList, TileDetail, TileList
+
+
+urlpatterns = [
+    path('tasks/', TaskList.as_view(), name='task_list'),
+    path('tasks/<uuid:task_id>/', TaskDetail.as_view(), name='task_detail'),
+    path('tiles/', TileList.as_view(), name='tile_list'),
+    path('tiles/<uuid:tile_id>/', TileDetail.as_view(), name='tile_detail'),
+]
