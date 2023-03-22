b9965d979e38fdcf4cdcdd67302769a2cdfc8cbd
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-22 16:54:35 +0000

    ADD: generic list and details views for Tile and Task serializers

--- task/views.py
+++ task/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from task.serializers import TaskSerializer, TileSerializer
+from task.models import Task, Tile, TaskType
+from rest_framework import generics
+from rest_framework import mixins
 
-# Create your views here.
+
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    lookup_url_kwarg = "task_id"
+
+
+class TileList(generics.ListCreateAPIView):
+    queryset = Tile.objects.all()
+    serializer_class = TileSerializer
+
+
+class TileDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TileSerializer
+    queryset = Tile.objects.all()
+    lookup_url_kwarg = "tile_id"
