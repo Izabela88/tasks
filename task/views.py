c41cc8108b8354d0028c6fbefaf8029afca32554
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-22 18:00:50 +0000

    ADD: permissions for Task and Tile views

--- task/views.py
+++ task/views.py
@@ -1,25 +1,30 @@
 from task.serializers import TaskSerializer, TileSerializer
 from task.models import Task, Tile
 from rest_framework import generics
+from rest_framework import permissions
 
 
 class TaskList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
     lookup_url_kwarg = "task_id"
 
 
 class TileList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Tile.objects.all()
     serializer_class = TileSerializer
 
 
 class TileDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     serializer_class = TileSerializer
     queryset = Tile.objects.all()
     lookup_url_kwarg = "tile_id"
