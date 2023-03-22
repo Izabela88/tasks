0d93e84dd517f0ac55e700d8dbef7ae2c71a0a4a
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-22 10:35:47 +0000

    ADD: register models in admin panel

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from task.models import Task, Tile, TaskType
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "order",
+        "description",
+        "tile_id",
+        "task_type",
+    ]
+    
+class TileAdmin(admin.ModelAdmin):
+    list_display = [
+        "status",
+        "launch_date",
+    ]
+    
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+    ]
+    
+    
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Tile, TileAdmin)
+admin.site.register(TaskType, TaskTypeAdmin)
