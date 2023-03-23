57f715b9277671c7325318ce746581542539f2a2
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-03-23 12:13:26 +0000

    ADD: tests for tiles

---
+++ tests/test_tiles.py
@@ -0,0 +1,57 @@
+import pytest
+from task.models import Tile
+
+
+@pytest.mark.django_db
+def test_get_tile_list(create_tile, authorized_client):
+    resp = authorized_client.get("/api/tiles/")
+    resp_json = resp.json()
+    assert resp.status_code == 200
+    assert len(resp_json) == 1
+    assert resp_json[0]["id"] == str(create_tile.id)
+
+
+@pytest.mark.django_db
+def test_get_tile_detail(create_tile, authorized_client):
+    resp = authorized_client.get(f"/api/tiles/{create_tile.id}/")
+    resp_json = resp.json()
+    assert resp.status_code == 200
+    assert resp_json["id"] == str(create_tile.id)
+
+
+@pytest.mark.django_db
+def test_delete_tile(create_tile, authorized_client):
+    tile = create_tile
+    url = f"/api/tiles/{create_tile.id}/"
+    resp = authorized_client.delete(url)
+    assert resp.status_code == 204
+    assert Tile.objects.filter(id=tile.id).exists() is False
+
+
+@pytest.mark.django_db
+def test_create_tile_successful(authorized_client):
+    data = {
+        "status": "PENDING",
+        "launch_date": "2023-03-22T15:51:53.565658Z",
+        "created_at": "2023-03-22T15:51:53.565624Z",
+        "modified_at": "2023-03-22T15:51:53.565643Z",
+    }
+    url = f"/api/tiles/"
+    assert len(Tile.objects.all()) == 0
+
+    resp = authorized_client.post(url, data=data)
+
+    assert resp.status_code == 201
+    assert len(Tile.objects.all()) == 1
+
+
+@pytest.mark.django_db
+def test_create_tile_unsuccessful(authorized_client):
+    data = {}
+    url = f"/api/tiles/"
+    assert len(Tile.objects.all()) == 0
+
+    resp = authorized_client.post(url, data=data)
+
+    assert resp.status_code == 400
+    assert len(Tile.objects.all()) == 0
